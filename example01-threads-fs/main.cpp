b613100e53c0c91156a883d0030cb079562d99ae
Carlos Eduardo Olivieri <caduolivieri1984@gmail.com>
2019-08-29 17:16:32 -0300

    Add files via upload

---
+++ example01-threads-fs/main.cpp
@@ -0,0 +1,28 @@
+#include "dirmgr.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	DirManager::check_dirs();
+	DirManager obj_dm;
+
+	if (! obj_dm.start_watching("./in"))
+	{
+		cout << "Could not start Directory Monitor." << endl;
+		return EXIT_FAILURE;
+	}
+
+	for(int n = 0; n <= 20; n++)
+	{
+		this_thread::sleep_for(chrono::seconds(2));
+		cout << "Main thread alive." << endl;
+	}
+
+	obj_dm.stop_watching();
+
+	cout << "Main thread finished." << endl;
+
+	return EXIT_SUCCESS;
+}
